7d75937eb573ed4c998b3cab406bad15e8a18223
IzeLeam <none>
2025-01-25 20:30:57 +0100

    fix: spawner command

--- src/main/java/dev/minelia/mineliashop/gui/ShopGui.java
+++ src/main/java/dev/minelia/mineliashop/gui/ShopGui.java
@@ -66,11 +66,9 @@ public class ShopGui extends BetterMenu {
               }
               removeMoney(player, config.getInt("items." + key + ".price"));
               if (key.contains("spawner")) {
-                System.out.println(config.getString("spawner_command").replace("%player%", player.getName()).replace("%type%", key).substring(0, key.indexOf("_")));
                 Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
-                    config.getString("spawner_command").replace("%player%", player.getName()).replace("%type%", key).substring(0, key.indexOf("_")));
+                    config.getString("spawner_command").replace("%player%", player.getName()).replace("%type%", key));
               } else {
-                System.out.println(config.getString("minelia_item_command").replace("%player%", player.getName()).replace("%item%", key));
                 Bukkit.getServer().dispatchCommand(Bukkit.getServer().getConsoleSender(),
                     config.getString("minelia_item_command").replace("%player%", player.getName()).replace("%item%", key));
               }
@@ -118,6 +116,7 @@ public class ShopGui extends BetterMenu {
         player.getInventory().removeItem(item);
       }
     }
+    throw new IllegalStateException("Player does not have enough money");
   }
 
   private Material getMaterial(String material) {
